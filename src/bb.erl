8a770d1eed66d6091e4c1891f1dc8105b52639f7
Uwe Dauernheim <uwe@klarna.com>
2011-12-22 22:06:33 +0100

    Refactor request and response handling

--- src/bb.erl
+++ src/bb.erl
@@ -1,63 +1,80 @@
-%% TODO Do standard wsdl parsing
+%%% TODO Do standard wsdl parsing
+%%% TODO Maybe use ibrowse for http handling https://github.com/cmullaparthi/ibrowse/
 -module(bb).
 
+%%%_* Exports ==========================================================
 -export([ start/0
         , get_node/1
-        , rest/1
-        , rest/2
-        , rest/3
         ]).
 
+%%%_* Defines ==========================================================
+-define(SERVER_URI,     "http://localhost:7474/").
+-define(DATA_PATH,      "db/data/").
+-define(SERVER_TIMEOUT, 3000).
 
+%%%_* Code =============================================================
+%%%_* API --------------------------------------------------------------
 start() ->
-  inets:start(),
-  {ok, Res} = rest(""),
-  ServerConfig = kf(<<"data">>, Res),
-  erlang:put(bb_server_path, erlang:binary_to_list(ServerConfig)),
-  ok.
+  ensure_started(inets),
+  ensure_server_running().
 
 get_node(Id) when is_integer(Id) ->
-  {ok, Res} = rest("node/"++erlang:integer_to_list(Id)),
-  kf(<<"data">>, Res).
+  {ok, Res} = rest(get, "node/"++erlang:integer_to_list(Id)),
+  kf(b("data"), Res).
 
-rest(Path) ->
-  rest(get, Path).
-
-rest(Method, Path) ->
-  Url = server_path() ++ Path,
-    case httpc:request(Method, {Url, []}, [], []) of
-      {ok,{{"HTTP/1.1",200,"OK"},_Headers,Body}} ->
-        Response = mochijson2:decode(Body, [{format, proplist}]),
-        {ok, Response};
-      {ok,{{"HTTP/1.1",404,"Not Found"},_Headers,Body}} ->
-        Response = mochijson2:decode(Body, [{format, proplist}]),
-        Message = kf(<<"message">>, Response),
-        {error, Message}
-    end.
-
-rest(Method, Path, _Payload) ->
-  Url = server_path() ++ Path,
-    case httpc:request(Method, {Url, []}, [], []) of
-      {ok,{{"HTTP/1.1",200,"OK"},_Headers,Body}} ->
-        Response = mochijson2:decode(Body, [{format, proplist}]),
-        {ok, Response};
-      {ok,{{"HTTP/1.1",404,"Not Found"},_Headers,Body}} ->
-        Response = mochijson2:decode(Body, [{format, proplist}]),
-        Message = kf(<<"message">>, Response),
-        {error, Message}
-    end.
-
-server_path() ->
-  case erlang:get(bb_server_path) of
-    undefined ->
-      {Protocol, Host, Port, Path} = default_path(),
-      Protocol++"://"++Host++":"++erlang:integer_to_list(Port)++Path;
-    Config ->
-      Config
+%%%_* Internals --------------------------------------------------------
+ensure_server_running() ->
+  try
+    {ok, Config} = rest(get, "/"),
+    _DataPath    = kf(b("data"), Config)
+  catch
+    throw:{error, econnrefused} ->
+      erlang:throw({error, server_not_running});
+    _:R ->
+      erlang:throw({error, R})
   end.
 
-default_path() ->
-  {"http", "localhost", 7474, "/"}.
+rest(Method, Path) ->
+  Request  = {path(Path), []},
+  Response = send_request(Method, Request),
+  parse_response(Response).
+
+rest(Method, Path, Data) ->
+  Payload  = percent:url_encode(Data),
+  Request  = {path(Path), [], "application/x-www-form-urlencoded", Payload},
+  Response = send_request(Method, Request),
+  parse_response(Response).
+
+send_request(Method, {Path, []}) ->
+  Url = ?SERVER_URI ++ Path,
+  httpc:request(Method, {Url, []}, [{timeout, ?SERVER_TIMEOUT}], []).
+ 
+parse_response({ok, {{"HTTP/1.1", 200, "OK"}, _Headers, Body}}) ->
+  Payload = mochijson2:decode(Body, [{format, proplist}]),
+  {ok, Payload};
+parse_response({ok, {{"HTTP/1.1", 404, "Not Found"}, Headers, Body}}) ->
+  case kf("content-type", Headers) of
+    "application/json" ->
+      io:format("BODY: ~p~n", [Body]),
+      Payload = mochijson2:decode(Body, [{format, proplist}]),
+      Message = kf(b("message"), Payload),
+      {error, Message};
+    _ ->
+      parse_response({error, invalid_method})
+  end;
+parse_response({error, Error}) ->
+  throw({error, Error}).
+
+path("/"++Path) -> Path;
+path(Path)      -> ?DATA_PATH++Path.
+
+%%%_* Helpers ----------------------------------------------------------
+ensure_started(App) ->
+  case application:start(App) of
+    ok                              -> ok;
+    {error, {already_started, App}} -> ok;
+    {error, _}=Error                -> throw(Error)
+  end.
 
 kf(Key, List) ->
   {Key, Value} = lists:keyfind(Key, 1, List),
@@ -68,3 +85,9 @@ kf(Key, List, Default) ->
     false        -> Default;
     {Key, Value} -> Value
   end.
+
+b(B) when is_binary(B) -> unicode:characters_to_list(B);
+b(L) when is_list(L)   -> unicode:characters_to_binary(L).
+
+%%% Mode: Erlang
+%%% End.
